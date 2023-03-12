89d66de8923e2f250b510b5425c50527456fd979
Emily Coleman <86796510+mlecoleman@users.noreply.github.com>
2023-03-11 21:47:57 -0500

    Initial commit for code lou capston project QA course 2

---
+++ TestTube/WebDriverFixture.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using OpenQA.Selenium.Chrome;
+using WebDriverManager;
+using WebDriverManager.DriverConfigs.Impl;
+
+namespace TestTube
+{
+    public class WebDriverFixture : IDisposable
+    {
+        public ChromeDriver ChromeDriver { get; private set; }
+
+        public WebDriverFixture()
+        {
+            var driver = new DriverManager().SetUpDriver(new ChromeConfig());
+            ChromeDriver = new ChromeDriver();
+        }
+
+
+        public void Dispose()
+        {
+            ChromeDriver.Quit();
+            ChromeDriver.Dispose();
+        }
+    }
+}
+

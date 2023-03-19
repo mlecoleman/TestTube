f9985aac30b187057be9f3b1878bcde9962f4d76
Emily Coleman <86796510+mlecoleman@users.noreply.github.com>
2023-03-18 21:06:18 -0400

    Write first test case for opening a new page and add POM files

---
+++ TestTube/PageObjectModels/PantsDotOrg.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using OpenQA.Selenium;
+
+namespace TestTube.PageObjectModels
+{
+	internal class PantsDotOrg
+	{
+		public string pantsDotOrgUrl = "https://www.pants.org/";
+
+        string gitTwitterButtonXpath = "//*[@id=\"social\"]//child::a[@title=\"pants.org Twitter\"]";
+		internal By gitTwitterButton { get => By.XPath(gitTwitterButtonXpath); }
+    }
+}
+
---
+++ TestTube/PageObjectModels/TwitterPage.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using OpenQA.Selenium;
+
+namespace TestTube.PageObjectModels
+{
+    internal class TwitterPage
+    {
+        internal string twitterUrl = "https://twitter.com/PalmerCCIE";
+
+        internal string twitterTitle = "Palmer Sample (@PalmerCCIE) / Twitter";
+
+        string gitTwitterUserUrlLinkXpath = "//*[@data-testid=\"UserUrl\"]//child::span[contains(text(), 'pants.org')]";
+        internal By gitTwitterUserUrlLink { get => By.XPath(gitTwitterUserUrlLinkXpath); }
+    }
+}
+

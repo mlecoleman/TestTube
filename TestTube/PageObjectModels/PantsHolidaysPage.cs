4b95e2f0e2db97535fc21df3f67a6efd641f644d
Emily Coleman <86796510+mlecoleman@users.noreply.github.com>
2023-03-24 18:11:55 -0400

    Add pom file for pants holidays pages

--- TestTube/PageObjectModels/PantsHolidaysPage.cs
+++ TestTube/PageObjectModels/PantsHolidaysPage.cs
@@ -1,11 +1,50 @@
-﻿using System;
+﻿using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium;
+using System;
+using WebDriverManager;
+using WebDriverManager.DriverConfigs.Impl;
+using Xunit;
+using Xunit.Abstractions;
+using FluentAssertions;
+using OpenQA.Selenium.Support.UI;
+using FluentAssertions.Execution;
+using TestTube.PageObjectModels;
+using TestTube.Tests;
+using AngleSharp.Dom;
+using System.Runtime.Intrinsics.X86;
+using System.Diagnostics.Metrics;
+
+
 namespace TestTube.PageObjectModels
 {
-	public class NoPantsDayPage
-	{
-		public NoPantsDayPage()
-		{
-		}
-	}
+    internal class PantsHolidaysPage
+    {
+        private readonly ChromeDriver Driver;
+
+        public PantsHolidaysPage(ChromeDriver driver)
+        {
+            Driver = driver;
+        }
+
+        internal string noPantsDayUrl = "https://www.timeanddate.com/holidays/fun/no-pants-day";
+
+        internal string takeYourPantsForAWalkDayUrl = "https://www.timeanddate.com/holidays/fun/take-your-pants-for-a-walk-day";
+
+        string walkYourPantsHeaderXpath = "//h1[contains(text(), \"Take your Pants for a Walk Day\")]";
+        internal By walkYourPantsHolidayHeader { get => By.XPath(walkYourPantsHeaderXpath); }
+
+        string monthYearDropdownId = "month";
+        internal By monthYearDropdown { get => By.Id(monthYearDropdownId); }
+
+        string july27thXpath = "//a[@href='/holidays/fun/take-your-pants-for-a-walk-day']";
+        internal By july27th { get => By.XPath(july27thXpath); }
+
+        internal void chooseJuly2023()
+        {
+            IWebElement monthYearDropdown = Driver.FindElement(By.Id("month"));
+            SelectElement monthYear = new SelectElement(monthYearDropdown);
+            monthYear.SelectByText("July 2023");
+        }
+    }
 }
 
